4a5b906fe9fd7721f8e33efd4c0b2a495f1295da
Irvzzz <silungan.patrickirvan2@gmail.com>
2023-03-16 15:38:03 +0800

    .

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -17,7 +17,7 @@ export class ProductTrackListingComponent implements OnInit {
     constructor(private _productService: ProductService) { }
 
     ngOnInit() {
-        this._productService.getAlbum(1).subscribe( response => this.albumInfo = response);
+        this._productService.getAlbum(1).subscribe( Response => this.albumInfo = Response);
     }
 
 }
