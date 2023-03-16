6ac878ff2c1ec4aec82236db31a67ade76042c7e
Irvzzz <silungan.patrickirvan2@gmail.com>
2023-03-16 15:52:00 +0800

    .

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,23 +1,20 @@
-import { Component, OnInit } from "@angular/core";
-
-import { ProductService } from "../product.service";
-
-import { Album } from "../album";
-
+import { Component, OnInit } from '@angular/core';
+import { Album } from '../album';
+import { ProductService } from '../product.service';
 
 @Component({
-    selector: 'app-product-tracklisting',
-    templateUrl: './product-tracklisting.component.html',
-    styleUrls: ['./product-tracklisting.component.css']
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
 })
+export class ProductTracklistingComponent implements OnInit {
 
-export class ProductTrackListingComponent implements OnInit {
+  albumInfo: Album;
 
-    albumInfo: Album;
-    constructor(private _productService: ProductService) { }
+  constructor(private _productService: ProductService) { }
 
-    ngOnInit() {
-        this._productService.getAlbum(1).subscribe( response => this.albumInfo = response);
-    }
+  ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
+  }
 
 }
